36dd35c7c5701f25f31e4a067381bc72bed2b023
Ranielm <138530934+BarrettWR@users.noreply.github.com>
2024-02-22 13:16:19 -0800

    Improved Gallery

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -22,10 +22,19 @@ let images = ref([
   },
   {
     title: '400x400',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus reprehenderit eveniet fuga, eligendi atque error dicta iusto hic modi, molestias rem sint consequuntur soluta autem, itaque fugit maiores vel.',
+    href: 'https://dummyimage.com/400/ffffff/000000',
+  },
+  {
+    title: '400x400',
     description: '400 x 400 Black',
     href: 'https://dummyimage.com/400/000000/ffffff',
   },
-  
+  {
+    title: '400x400',
+    description: '400 x 400 White',
+    href: 'https://dummyimage.com/400/ffffff/000000',
+  },
 ])
 
 let index = ref(null)
@@ -42,15 +51,19 @@ function linkFunction() {
             <template #title>Work</template>
         </Topbar>
         <div class="max-w-[100%] h-[80svh] overflow-y-auto p-20">
-            <div class="flex flex-wrap place-content-center max-w-[100%]">
+            <div class="flex flex-wrap place-content-center max-w-[100%] ">
                 <VueGallery class="" description="hello" :images="images" :index="index" @close="index = null"></VueGallery>
-                <div
-                    class="image"
-                    v-for="(image, imageIndex) in images"
-                    :key="imageIndex"
-                    @click="index = imageIndex"
-                    :style="{ backgroundImage: 'url(' + image.href + ')', width: '350px', height: '250px' }"
-                ></div>
+                <div class="sizing flex grow flex-wrap place-content-center">
+                    <div
+                      class="image"
+                      v-for="(image, imageIndex) in images"
+                      :key="imageIndex"
+                      @click="index = imageIndex"
+                      :style="{ backgroundImage: 'url(' + image.href + ')', }"
+                  ></div>
+                </div>
+                
+                
             </div>
         </div>
     </div>
@@ -82,4 +95,17 @@ function linkFunction() {
     margin: 50px;
     margin-top: 70px;
   }
+
+
+  .sizing > :nth-child(-n+4) {
+    width: 80%;
+    height: 500px;
+  }
+
+  .sizing > :nth-child(n+4) {
+    width: 39%;
+    height: 250px;
+  }
+
+
 </style>
